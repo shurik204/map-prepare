12762c49125fbe83e267b0b8088dbb75ed25c69e
shurik204 <43310372+shurik204@users.noreply.github.com>
2021-11-16 14:31:43 +0200

    Switch to multiprocessing

--- map_prepare/modules/remove_empty_chunks.py
+++ map_prepare/modules/remove_empty_chunks.py
@@ -3,10 +3,12 @@ __group__ = 'regions'
 
 from map_prepare.lib import logger, utils
 from map_prepare.lib.config import config
+from multiprocessing import JoinableQueue
 from nbt import nbt, region
-import threading
+from queue import Empty
+import multiprocessing
 import anvil
-import queue
+import time
 import io
 import os
 
@@ -24,132 +26,152 @@ def palette_exists(section: nbt.TAG_Compound):
         return True
     except KeyError: return False
 
-class RegionProcessor(threading.Thread):
-    def __init__(self, queue: queue.Queue):
-        super().__init__(name=f'RegionProcessor-{threading.active_count()-1}')
-        self.queue = queue
+class RegionProcessor(multiprocessing.Process):
+    def __init__(self, q: JoinableQueue):
+        super().__init__(name=f'RegionProcessor-{multiprocessing.active_children().__len__()-1}', args=(q, ), target=self.actual_task)
+        self.q = q
 
         self.start()
 
-    def run(self):
-        try:
-            while True:
-                # Poll queue every second for a new task
-                self.actual_task(self.queue.get(timeout=1))
-        except queue.Empty:
-            # If queue is empty, terminate the thread
-            # Self-recycle :)
-            del self
+    def actual_task(self, q):
+        while True:
+            try:
+                # Poll queue every 0.1 second for a new task
+                region_path = q.get(timeout=0.1)
+            except Empty:
+                # If queue is empty, terminate the thread
+                # Self-recycle :)
+                return
+            with open(region_path, 'rb') as region_file:
+                region_io = io.BytesIO(region_file.read())
 
-    def actual_task(self, region_path):
-        with open(region_path, 'rb') as region_file:
-            region_io = io.BytesIO(region_file.read())
+            reg = region.RegionFile(fileobj=region_io)
+            anv_reg = anvil.Region.from_file(region_path)
 
-        reg = region.RegionFile(fileobj=region_io)
-        anv_reg = anvil.Region.from_file(region_path)
+            # Empty region
+            new_region_io = io.BytesIO()
+            new_reg = region.RegionFile(fileobj=new_region_io)
 
-        # Empty region
-        new_region_io = io.BytesIO()
-        new_reg = region.RegionFile(fileobj=new_region_io)
+            logger.info(f'Processing region "{region_path}" with {reg.chunk_count()} chunk{"s" if reg.chunk_count() != 1 else ""}')
+            for x in range(32):
+                for z in range(32):
+                    # Make sure to clear the previous chunk
+                    chunk = None
+                    anv_chunk = None
 
-        logger.info(f'Processing region "{region_path}" with {reg.chunk_count()} chunk{"s" if reg.chunk_count() != 1 else ""}')
-        for x in range(32):
-            for z in range(32):
-                # Make sure to clear the previous chunk
-                chunk = None
-                anv_chunk = None
-
-                try:
-                    chunk = reg.get_nbt(x, z)
-                    anv_chunk = anv_reg.get_chunk(x, z)
-                # No chunk: just skip
-                except region.InconceivedChunk: pass
-                # Chunk has length of 0: remove it.
-                except region.ChunkHeaderError: reg.unlink_chunk(x, z)
-                # Added KeyError to prevent crash because TileEntities is missing (ToG is a weird map)
-                except (anvil.chunk.ChunkNotFound, KeyError): pass
-
-                if chunk != None and anv_chunk != None:
-                    empty_chunk = True
-
-                    # Assume that it can be 1.17, where entities
-                    # were moved into another region-like file
-                    no_entites = True
                     try:
-                        # Pre 1.14 I guess?
-                        if isinstance(chunk['Level']['Entities'], nbt.TAG_Long_Array):
-                            no_entites = chunk['Level']['Entities'].__len__() == 0
-                        else: no_entites = chunk['Level']['Entities'].tagID == 0
-                    except KeyError: pass
+                        chunk = reg.get_nbt(x, z)
+                        anv_chunk = anv_reg.get_chunk(x, z)
+                    # No chunk: just skip
+                    except region.InconceivedChunk: pass
+                    # Chunk has length of 0: remove it.
+                    except region.ChunkHeaderError: reg.unlink_chunk(x, z)
+                    # Added KeyError to prevent crash because TileEntities is missing (ToG is a weird map)
+                    except (anvil.chunk.ChunkNotFound, KeyError): pass
 
-                    no_sections = True
-                    try:
-                        if isinstance(chunk['Level']['Sections'], nbt.TAG_Long_Array):
-                            no_sections = chunk['Level']['Sections'].__len__() == 0
-                        else: no_sections = chunk['Level']['Sections'].tagID == 0
-                    except KeyError: pass
+                    if chunk != None and anv_chunk != None:
+                        empty_chunk = True
 
-                    if not no_sections:
-                        # Try checking this chunk by looking for tile entities (chests, barrels, command blocks, etc.)
-                        try: empty_chunk = chunk['Level']['TileEntities'].tagID == 0
+                        # Assume that it can be 1.17, where entities
+                        # were moved into another region-like file
+                        no_entites = True
+                        try:
+                            # Pre 1.14 I guess?
+                            if isinstance(chunk['Level']['Entities'], nbt.TAG_Long_Array):
+                                no_entites = chunk['Level']['Entities'].__len__() == 0
+                            else: no_entites = chunk['Level']['Entities'].tagID == 0
                         except KeyError: pass
-                        # Actually heavy check
-                        if empty_chunk:
-                            # Chunks are stored in 16x16x16 sections from 0 to 15.
-                            # For every section that exists in this chunk
-                            for section in chunk['Level']['Sections']:
-                                if palette_exists(section):
-                                    empty_chunk = is_empty_section(anv_chunk, section)
-                                    if not empty_chunk: break
 
-                    if empty_chunk and no_entites:
-                        # Remove empty chunk
-                        reg.unlink_chunk(x, z)
-                    else:
-                        new_reg.write_chunk(x, z, chunk)
+                        no_sections = True
+                        try:
+                            if isinstance(chunk['Level']['Sections'], nbt.TAG_Long_Array):
+                                no_sections = chunk['Level']['Sections'].__len__() == 0
+                            else: no_sections = chunk['Level']['Sections'].tagID == 0
+                        except KeyError: pass
 
-        # Check if there is anything left
-        # If region is empty:
-        if new_reg.chunk_count() == 0:
-            logger.debug(f'Deleted empty region file "{region_path}"')
-            # Just remove the file
-            utils.delete_file(region_path)
-        else:
-            logger.debug(f'Writing region "{region_path}" with {reg.chunk_count()} chunks')
-            # Write changes to file
-            with open(region_path, 'wb') as region_file:
-                # Make sure to seek to the beginning of the BytesIO
-                new_region_io.seek(0)
-                # And then write data to disk
-                region_file.write(new_region_io.read())
+                        if not no_sections:
+                            # Try checking this chunk by looking for tile entities (chests, barrels, command blocks, etc.)
+                            try: empty_chunk = chunk['Level']['TileEntities'].tagID == 0
+                            except KeyError: pass
+                            # Actually heavy check
+                            if empty_chunk:
+                                # Chunks are stored in 16x16x16 sections from 0 to 15.
+                                # For every section that exists in this chunk
+                                for section in chunk['Level']['Sections']:
+                                    if palette_exists(section):
+                                        empty_chunk = is_empty_section(anv_chunk, section)
+                                        if not empty_chunk: break
 
-        self.queue.task_done()
+                        if empty_chunk and no_entites:
+                            # Remove empty chunk
+                            reg.unlink_chunk(x, z)
+                        else:
+                            new_reg.write_chunk(x, z, chunk)
+
+            # Check if there is anything left
+            # If region is empty:
+            if new_reg.chunk_count() == 0:
+                logger.debug(f'Deleted empty region file "{region_path}"')
+                # Just remove the file
+                utils.delete_file(region_path)
+            else:
+                logger.debug(f'Writing region "{region_path}" with {reg.chunk_count()} chunks')
+                # Write changes to file
+                with open(region_path, 'wb') as region_file:
+                    # Make sure to seek to the beginning of the BytesIO
+                    new_region_io.seek(0)
+                    # And then write data to disk
+                    region_file.write(new_region_io.read())
+
+            q.task_done()
 
 
 def main(world_path: str):
     if not config['settings']['remove_empty_chunks']:
         return
 
-    q = queue.Queue()
+    q = JoinableQueue()
 
     logger.info('Removing empty chunks')
 
+    logger.info('Counting chunks')
+    total_chunks = 0
+    total_files = 0
     # Moved region folders search to utils
     region_folders = utils.get_all_region_folders(world_path)
     
     for region_folder_path in region_folders:
         # logger.info(f'Processing folder "{region_folder_path}"')
 
-        for region_path in os.listdir(region_folder_path):
-            # Construct path to region file
-            region_path = f'{region_folder_path}/{region_path}'
+        for region_name in os.listdir(region_folder_path):
+            # Filter out bad chunks
+            try:
+                # Construct path to region file
+                region_path = f'{region_folder_path}/{region_name}'
+                # Counters
+                total_chunks += region.RegionFile(region_path).chunk_count()
+                total_files += 1
 
-            q.put(str(region_path))
+                q.put(str(region_path))
+            except region.MalformedFileError:
+                # Just delete that
+                utils.delete_file(region_path)
 
+    logger.info(f'Found {total_chunks} chunks in {total_files} region files')
+
+    # multiprocessing.set_start_method('spawn')
     threads = []
     for _ in range(config['threads']):
         threads.append(RegionProcessor(q))
+        
+    files_left = q.qsize()
+    while (q.qsize()):
+        if files_left != q.qsize():
+            logger.info(f'Processing progress: {total_files - q.qsize()}/{total_files}')
+            files_left = q.qsize()
 
+    time.sleep(1)
+    logger.info('Waiting for threads to finish')
     q.join()
 
     del threads
